83ec940baf6d2706c86aa4b2f1e3168aceb7372d
d.usova1 <d.usova1@outlook.com>
2020-01-07 17:44:33 +0700

    add .idea tio gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] argv) {
-        int port = 100;
+        System.out.println("Enter port number");
+        Scanner in = new Scanner(System.in);
+        int port = in.nextInt();
+
         Proxy proxy = new Proxy();
         proxy.run(port);
     }
